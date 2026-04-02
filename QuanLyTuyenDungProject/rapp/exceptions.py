3743f659cc5eb1ae641181c40a7b430aaf126e9a
nhulop6c@gmail.com <vonhu2875@gmail.com>
2026-04-02 22:35:25 +0700

    Tạo trang register và login

---
+++ QuanLyTuyenDungProject/rapp/exceptions.py
@@ -0,0 +1,7 @@
+class ValidationError(Exception):
+    # Lỗi do người dùng nhập sai định dạng
+    pass
+
+class DuplicateError(Exception):
+    # Lỗi do bị trùng dữ liệu
+    pass
--- QuanLyTuyenDungProject/rapp/index.py
+++ QuanLyTuyenDungProject/rapp/index.py
@@ -1,10 +1,15 @@
 import math
 
-from flask import render_template, request
+from flask import render_template, request, redirect
+from flask_login import login_user, logout_user
+
 from rapp import dao, app, login
+from rapp.dao import add_user
+from rapp.exceptions import ValidationError, DuplicateError
+from rapp.models import User, UserRole
 
 
-@app.route("/")
+@app.route('/')
 def index():
     cate_id = request.args.get('category_id')
     kw = request.args.get('kw')
@@ -13,6 +18,53 @@ def index():
     jobs = dao.load_jobs(cate_id, kw, page)
     return render_template("index.html", jobs=jobs, categories=categories,
                            pages=math.ceil(dao.count_jobs(cate_id) / app.config['PAGE_SIZE']))
+
+@app.route('/login')
+def login_view():
+    return render_template('login.html')
+
+@app.route('/login', methods=['post'])
+def login_process():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = dao.auth_user(username=username, password=password)
+    if user:
+        login_user(user=user)
+
+    next = request.args.get('next')
+    return redirect(next if next else '/')
+
+@app.route('/logout')
+def logout_process():
+    logout_user()
+    return redirect('/login')
+@app.route('/register')
+def register_view():
+    users_show = [UserRole.EMPLOYER, UserRole.CANDIDATE]
+    return render_template('register.html', UserRole = users_show)
+
+@app.route('/register', methods=['post'])
+def register_process():
+    users_show = [UserRole.EMPLOYER, UserRole.CANDIDATE]
+    data = request.form
+
+    password = data.get("password")
+    confirm = data.get("confirm")
+    if password != confirm:
+        err_msg = "Mật khẩu không khớp!"
+        return render_template('register.html', err_msg=err_msg,UserRole = users_show)
+    try:
+        add_user(name=data.get('name'), username=data.get('username'), password=password, avatar=request.files.get('avatar'),
+                 email=data.get('email'), phone=data.get('phone'), user_role=data.get('user_role'))
+        return redirect('/login')
+    except ValidationError as val:
+        return render_template('register.html', err_msg=str(val), UserRole = users_show)
+    except DuplicateError as dup:
+        return render_template('register.html', err_msg=str(dup), UserRole = users_show)
+    except Exception as ex:
+        return render_template('register.html', err_msg=str(ex), UserRole = users_show)
+
 
 
 @login.user_loader

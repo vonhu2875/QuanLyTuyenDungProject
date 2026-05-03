71d9d484c5c612ae8a47e70202bfe4176fa9eb54
nhulop6c@gmail.com <vonhu2875@gmail.com>
2026-05-03 21:37:22 +0700

    Chỉnh sửa lại front-end

--- QuanLyTuyenDungProject/rapp/static/main.js
+++ QuanLyTuyenDungProject/rapp/static/main.js
@@ -1,31 +1,15 @@
-//// 1. Phải để hàm này ở NGOÀI CÙNG của file để HTML gọi được
-//function updateFileName(input) {
-//    const display = document.getElementById('file-name-display');
-//    if (input.files && input.files[0]) {
-//        display.innerHTML = `<i class="bi bi-check-circle-fill me-1"></i> Đã chọn: ${input.files[0].name}`;
-//    }
-//}
-//
+//Có 1 số trang tui không cần phải tự động tắt sa
+//u 3 giây -> Hà có cần thì đặt tên 1 selector khác để set lại 3 giây ngen
 //document.addEventListener("DOMContentLoaded", function() {
-//    // 2. Xử lý lỗi ảnh avatar
-//    document.querySelectorAll('.user-avatar').forEach(img => {
-//        img.addEventListener('error', function() {
-//            this.style.display = 'none';
-//            if (this.nextElementSibling) {
-//                this.nextElementSibling.style.display = 'block';
-//            }
-//        });
-//    });
-//
-//    // 3. Tự động đóng TẤT CẢ Alert sau 3 giây (Đã sửa lỗi không hoạt động)
-//    setTimeout(function() {
-//        var alerts = document.querySelectorAll('.alert');
-//        alerts.forEach(function(alertElement) {
-//            if (typeof bootstrap !== 'undefined') {
-//                // Sử dụng getOrCreateInstance để đảm bảo đóng được mọi loại thông báo
-//                var bsAlert = bootstrap.Alert.getOrCreateInstance(alertElement);
-//                if (bsAlert) bsAlert.close();
-//            }
-//        });
-//    }, 3000);
-//});
+//        // Tự tắt thông báo sau 3 giây cho toàn bộ các trang
+//        setTimeout(function() {
+//            const alerts = document.querySelectorAll('.alert');
+//            alerts.forEach(function(a) {
+//                if (typeof bootstrap !== 'undefined') {
+//                    const bsA = bootstrap.Alert.getOrCreateInstance(a);
+//                    if (bsA) bsA.close();
+//                } else {
+//                    a.style.display = 'none';
+//                }
+//            });
+//        }, 3000);

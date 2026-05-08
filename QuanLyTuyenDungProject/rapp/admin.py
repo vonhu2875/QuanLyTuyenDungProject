c72b946b88e688d39bdb05d286766ce5f157f015
thaihee <nguyendiepthaiha@gmail.com>
2026-05-09 02:42:54 +0700

    Delete QuanLyTuyenDungProject/rapp/admin.py

--- QuanLyTuyenDungProject/rapp/admin.py
+++
@@ -1,7 +0,0 @@
-# from flask_admin.contrib.sqla import ModelView
-# from flask_login import current_user
-# from rapp.models import UserRole
-#
-# class AdminView(ModelView):
-#     def is_accessible(self):
-#         return current_user.is_authenticated and current_user.user_role == UserRole.ADMIN

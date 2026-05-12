c7a959b6aac972f1d12661aa92e30f6203209d39
nhulop6c@gmail.com <vonhu2875@gmail.com>
2026-05-12 22:08:50 +0700

    Chỉnh sửa unit test api

--- QuanLyTuyenDungProject/rapp/test/test_add_job_api.py
+++ QuanLyTuyenDungProject/rapp/test/test_add_job_api.py
@@ -1,3 +1,4 @@
+from rapp.exceptions import ValidationError, DuplicateError
 from rapp.test.test_base import test_client, test_app
 from rapp.dao import UserRole
 
@@ -9,7 +10,7 @@ def test_create_job_success(test_client, mocker):
     mocker.patch('flask_login.utils._get_user', return_value=FakeUser())
     mocker.patch('flask_login.current_user', return_value=FakeUser())
 
-    mock_add = mocker.patch("rapp.index.add_job")
+    mock_add = mocker.patch("rapp.dao.add_job")
 
     res = test_client.post('/jobs', data={
         'title': 'Lập trình viên Python Senior',
@@ -34,7 +35,7 @@ def test_create_job_exception(test_client, mocker):
     mocker.patch('flask_login.current_user', return_value=FakeUser())
 
 
-    mock_add = mocker.patch("rapp.index.add_job", side_effect=Exception('Tiêu đề phải ít nhất 10 ký tự'))
+    mock_add = mocker.patch("rapp.dao.add_job", side_effect=Exception('DB Error'))
     mock_render = mocker.patch("rapp.index.render_template", return_value="HTML gia lap")
     res = test_client.post('/jobs', data={
         'title': 'a'*5,
@@ -47,10 +48,10 @@ def test_create_job_exception(test_client, mocker):
     assert "HTML gia lap" in res.get_data(as_text=True)
     mock_render.assert_called_once()
     args, kwargs = mock_render.call_args
-    assert kwargs['err_msg'] == 'Tiêu đề phải ít nhất 10 ký tự'
+    assert kwargs['err_msg'] == 'DB Error'
     mock_add.assert_called_once()
 
-
+#ValidationError
 def test_create_job_unauthorized(test_client, mocker):
     class FakeUser():
         is_authenticated = True
@@ -60,7 +61,8 @@ def test_create_job_unauthorized(test_client, mocker):
     mocker.patch('flask_login.utils._get_user', return_value=FakeUser())
     mocker.patch('flask_login.current_user', return_value=FakeUser())
 
-    mock_render = mocker.patch("rapp.index.render_template", return_value="Lỗi quyền")
+    mock_add = mocker.patch("rapp.dao.add_job", side_effect=ValidationError('Loi quyen truy cap'))
+    mock_render = mocker.patch("rapp.index.render_template", return_value="HTML gia lap")
 
     res = test_client.post('/jobs', data={
         'title': 'a' * 5,
@@ -70,6 +72,35 @@ def test_create_job_unauthorized(test_client, mocker):
         'category_id': '1'
     })
     assert res.status_code == 200
+    assert "HTML gia lap" in res.get_data(as_text=True)
     mock_render.assert_called_once()
     args, kwargs = mock_render.call_args
-    assert "Bạn không có quyền đăng tin!" in kwargs['err_msg']
+    assert kwargs['err_msg'] == 'Loi quyen truy cap'
+    mock_add.assert_called_once()
+
+#DuplicateError
+def test_create_job_duplicated(test_client, mocker):
+    class FakeUser():
+        is_authenticated = True
+        id = 2
+        user_role = UserRole.CANDIDATE
+
+    mocker.patch('flask_login.utils._get_user', return_value=FakeUser())
+    mocker.patch('flask_login.current_user', return_value=FakeUser())
+
+    mock_add = mocker.patch("rapp.dao.add_job", side_effect=DuplicateError('Loi trung tin tuyen dung'))
+    mock_render = mocker.patch("rapp.index.render_template", return_value="HTML gia lap")
+
+    res = test_client.post('/jobs', data={
+        'title': 'a' * 5,
+        'description': '...',
+        'salary': '1000',
+        'deadline': '2026-12-31',
+        'category_id': '1'
+    })
+    assert res.status_code == 200
+    assert "HTML gia lap" in res.get_data(as_text=True)
+    mock_render.assert_called_once()
+    args, kwargs = mock_render.call_args
+    assert kwargs['err_msg'] == 'Loi trung tin tuyen dung'
+    mock_add.assert_called_once()

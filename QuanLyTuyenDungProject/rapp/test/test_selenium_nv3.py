ce51e7506a4a5c1da94d34e2e48b13534e40361f
thaihee <nguyendiepthaiha@gmail.com>
2026-05-13 19:24:47 +0700

    Locust va Selenium 2

--- QuanLyTuyenDungProject/rapp/test/test_selenium_nv3.py
+++ QuanLyTuyenDungProject/rapp/test/test_selenium_nv3.py
@@ -32,40 +32,7 @@ def test_manage_applications(driver):
     driver.save_screenshot(os.path.join(current_dir, "nv3_1_manage_apps.png"))
 
 
-# 2. Test quản lý tin đăng
-def test_manage_jobs(driver):
-    login = LoginPage(driver)
-    manage_job = ManageJobPage(driver)
-
-    login.open_page()
-    login.login('employer1', '123456')
-    time.sleep(2)
-
-    manage_job.open_page()
-    time.sleep(1)
-
-    # Test Toggle
-    manage_job.click_toggle()
-    time.sleep(2)
-
-    # Test Edit
-    manage_job.click_edit()
-    time.sleep(1)
-    title_input = driver.find_element(By.NAME, "title")
-    title_input.clear()
-    title_input.send_keys("Lập trình viên Python")
-
-    submit_btn = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-    driver.execute_script("arguments[0].scrollIntoView();", submit_btn)
-    time.sleep(0.5)
-    driver.execute_script("arguments[0].click();", submit_btn)
-    time.sleep(2)
-
-    assert "/users/jobs" in driver.current_url
-    driver.save_screenshot(os.path.join(current_dir, "nv3_2_manage_jobs.png"))
-
-
-# 3. Test bảo mật
+# 2. Test bảo mật
 def test_security(driver):
     login = LoginPage(driver)
 
@@ -87,4 +54,4 @@ def test_security(driver):
     time.sleep(1)
     assert "403" in driver.page_source or "Lỗi quyền truy cập" in driver.page_source
 
-    driver.save_screenshot(os.path.join(current_dir, "nv3_3_security.png"))
+    driver.save_screenshot(os.path.join(current_dir, "nv3_2_security.png"))
